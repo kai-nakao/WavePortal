9b73e9b203a5dff3a466fed8ac528cb1eb55a568
kai-nakao <nkkai0803@gmail.com>
2022-06-22 12:10:07 +0900

    update run.js

--- scripts/run.js
+++ scripts/run.js
@@ -1,11 +1,17 @@
-const { hexStripZeros } = require('ethers/lib/utils')
-
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigner()
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal')
   const waveContract = await waveContractFactory.deploy()
   await waveContract.deployed()
 
   console.log('Contract deployed to:', waveContract.address)
+  console.log('Contract deployed by:', owner.address)
+
+  let waveCount
+  waveCount = await waveContract.getTotalWaves()
+
+  let waveTxn = await waveContract.wave()
+  await waveTxn.wait()
 }
 
 const runMain = async () => {
