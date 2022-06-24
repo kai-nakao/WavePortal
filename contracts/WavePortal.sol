c2ab3749a9d10b957881f9f692b87ab6b7d00191
kai-nakao <nkkai0803@gmail.com>
2022-06-24 14:30:58 +0900

    update contract code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,19 +4,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
-	uint256 totalSum;
+	uint256 totalWaves;
 
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
     }
 
-	function purchase() public {
-		totalSum += 110;
-		console.log("%s has purchased!", msg.sender);
+	function wave() public {
+		totalWaves += 1;
+		console.log("%s has waved!", msg.sender);
 	}
 
-	function getTotalSum() public view returns (uint256) {
-		console.log("we have %d total Sum!", totalSum);
-		return totalSum;
+	function getTotalWaves() public view returns (uint256) {
+		console.log("we have %d total Wave!", totalWaves);
+		return totalWaves;
 	}
 }
