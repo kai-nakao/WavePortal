69d8b22df04b0aba54bff85bc0133650b9d56e66
kai-nakao <nkkai0803@gmail.com>
2022-06-29 06:40:19 +0900

    create random system

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,37 +4,56 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
-	uint256 totalWaves;
+    uint256 totalWaves;
 
-	event NewWave(address indexed from, uint256 timestamp, string message);
+    uint256 private seed;
 
-	struct Wave {
-		address waver;
-		string message;
-		uint256 timestamp;
-	}
+    event NewWave(address indexed from, uint256 timestamp, string message);
 
-	Wave[] waves;
-
-    constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
     }
 
-	function wave(string memory _message) public {
-		totalWaves += 1;
-		console.log("%s waved w/ message %s", msg.sender, _message);
+    Wave[] waves;
 
-		waves.push(Wave(msg.sender, _message, block.timestamp));
+    constructor() payable {
+        console.log("We have been constructed!");
 
-		emit NewWave(msg.sender, block.timestamp, _message);
-	}
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
 
-	function getAllWaves() public view returns (Wave[] memory) {
-		return waves;
-	}
+    function wave(string memory _message) public {
+        totalWaves += 1;
+        console.log("%s waved w/ message %s", msg.sender, _message);
 
-	function getTotalWaves() public view returns (uint256) {
-		console.log("We have %d total waves!", totalWaves);
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        seed = (block.timestamp + block.difficulty + seed) % 100;
+
+        console.log("Random # generated: %d", seed);
+
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
         return totalWaves;
-	}
+    }
 }
